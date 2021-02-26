182eb4f6d02eb75ec90d233841d7c3d4b4f7a877
Gary Jiménez <garyjimenez0102@gmail.com>
2021-02-25 20:48:11 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 600,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('./index.html')
+}
+
+app.whenReady().then(createWindow)
